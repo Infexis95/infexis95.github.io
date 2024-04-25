a3fe3479f1b5e1695dbe8a8201bbc33f426d4211
Infexis <jeremy.jonsson@hotmail.com>
2024-04-26 00:21:45 +0200

    Added sidebar animation, fixed centering of clock.

--- sub-js/infexisJS.js
+++ sub-js/infexisJS.js
@@ -1,3 +1,14 @@
+const sideBar = document.querySelector('.side-bar');
+const homeButton = document.querySelector('.homepage-button');
+
+homeButton.addEventListener('mouseover', () => {
+    sideBar.classList.add('show');  /* Add 'show' class on hover */
+  });
+  
+  homeButton.addEventListener('mouseout', () => {
+    sideBar.classList.remove('show'); /* Remove 'show' class on mouseout */
+  });
+
 function startTime() {
     const localDate = new Date()
     let hour = localDate.getHours()
@@ -13,3 +24,4 @@ function adjustTime(i) {
     if (i < 10) {i = "0" + i}
     return i;
 }
+
