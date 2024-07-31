ab92e9db3996c45938030b2c1a9a9cb292361923
Derpious <weasel998@live.com>
2024-08-01 00:02:03 +0200

    Begining my little page instead of fucking around lol

--- scripts/Derpious/DerpiousJS.js
+++ scripts/Derpious/DerpiousJS.js
@@ -1,7 +1,11 @@
-const ClickMe_js = document.getElementById("Click_Me"); // saves the 'Click Me' button in the ClickMe_js variable.
+const ClickMe_js = document.getElementsByClassName("genST"); // saves the 'Click Me' button in the ClickMe_js variable.
 const Result_js = document.getElementById("Result"); // Same for the Result paragraph
+const Addup_js = document.getElementById("addup")
 
-ClickMe_js.addEventListener("click", e => {   // I don't know why js is stupid like this, but this is how you add an event listener.
-    Result_js.innerHTML = 'Test!'; // Sets the inner HTML of the element connected to the Result_js variable to "Test!"
-})
+function roll2d6() {
+   return Math.floor(Math.random() * (12 - 2 + 1)) + 2;
+}
 
+function r2Dresult() {
+   
+}
