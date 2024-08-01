44c2d96c28127f67d545494437845ae317dd6142
Derpious <weasel998@live.com>
2024-08-01 23:45:27 +0200

    Anotehr day of workrwork

--- scripts/Derpious/DerpiousJS.js
+++ scripts/Derpious/DerpiousJS.js
@@ -1,28 +1,13 @@
 const ClickMe_js = document.getElementsByClassName("genST"); // saves the 'Click Me' button in the ClickMe_js variable.
 const Result_js = document.getElementById("Result"); // Same for the Result paragraph
 const STType_js = document.getElementById("sttype")
+const test_js = document.getElementsByClassName("result_box")
 
 function roll2d6() {
-    return Math.floor(Math.random() * (12 - 2 + 1)) + 2;
+   return Math.floor(Math.random() * (12 - 2 + 1)) + 2;
 }
 
 function r2Dresult() {
-    document.querySelectorAll('.result_box').forEach(el => {el.innerHTML = roll2d6();})
+   document.querySelectorAll('.result_box').forEach(el => { el.innerHTML = roll2d6(); })
 }
 
-function startype() {
-    x = roll2d6();
-    STType_js.innerHTML = x;
-}
-
-let a, b, c; {
-    a = 5;
-    b = 6;
-    c = a + b;
-    document.getElementById("demo1").innerHTML = c;
-}
-
-function backupr2Dresult() {
-   result = roll2d6();
-   Result_js.innerHTML = result;
-}
