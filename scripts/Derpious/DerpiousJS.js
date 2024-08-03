f3cb51ab6fc7052b53996d66d80a990c99e1dbd4
Derpious <weasel998@live.com>
2024-08-03 12:05:09 +0200

    Added a functional star gen

--- scripts/Derpious/DerpiousJS.js
+++ scripts/Derpious/DerpiousJS.js
@@ -1,31 +1,68 @@
-const ClickMe_js = document.getElementById("genST"); // saves the 'Click Me' button in the ClickMe_js variable.
-const Result_js = document.getElementById("Result"); // Same for the Result paragraph
-const STType_js = document.getElementById("sttype");
-const test_js = document.getElementById("result_box");
-const roll2d = (Math.floor(Math.random() * (12 - 2 + 1)) + 2);
-let SLC_V = " V"
+function STType() {
 
-function roll2d6() {
-   return (Math.floor(Math.random() * (12 - 2 + 1)) + 2);
-}
+   // Generate random number between 0 and 1.
+   let x = Math.random();
+   let y = Math.random();
 
-function r2Dresult() {
-   document.querySelectorAll('#result_box').forEach(el => { el.innerHTML = roll2d6() + "" + roll2d6() + SLC_V; });
-}
-
-function val_transf() {
-   Math.random()              //I realised that rolling 2d6 to determine value adds extra and unnecessary complexity. The whole reason for rolling the 2d6
-   let x = Math.random()      //is to generate a random number between 2 and 12. Aka 11 different numbers. Dividing the 4 types based on their frequency,
-   if (x < 0.45) {            //I get a percentage number which I can randomly generate via math.random()
-      document.getElementById("result_box2").innerHTML = "M";  
-   } else if (x > 0.45, x < 0.67) {
-      document.getElementById("result_box2").innerHTML = "K";   // example: roll 2d6. a roll of 3, 4, 5, and 6 gives you M. 4 / 9 is roughly 45%.
-   } else if (x > 0.67, x < 0.89) {                             // a roll of 7 or 8 gives you K. 22%. Same goes for G, etc.
-      document.getElementById("result_box2").innerHTML = "G";   // 
-   } else if (x > 0.89, x < 1) {
-      document.getElementById("result_box2").innerHTML = "F";
+   // compare generated 'x' value to apply star type.
+   let result_type;
+   if (x < 0.091) {
+      result_type = "Special";
+   } else if (x > 0.091 && x < 0.454) {
+      result_type = "M";
+   } else if (x > 0.454 && x < 0.636) {
+      result_type = "K";   // 
+   } else if (x > 0.636 && x < 0.818) {
+      result_type = "G";
+   } else if (x > 0.818 && x < 0.909) {
+      result_type = "F";
+   } else if (x > 0.909 && x < 1.000) {
+      result_type = "Hot";
    }
-      document.getElementById("result_box3").innerHTML = x;
+
+   // Star type "special" and "hot" are different from ordinary, so I'll updated that in future.
+   if (x < 0.091 || x > 0.909 && x < 1.000) {
+      document.getElementById("result_box").innerHTML = "Error"
+      console.log("startype:", result_type, "(not yet supported.)")
+      return
+   }
+
+   // compare generated 'y' value to apply star subtype.
+   let result_subtype;
+   if (y < 0.182) {
+      result_subtype = "0";
+   } else if (y > 0.182 && y < 0.273) {
+      result_subtype = "1";
+   } else if (y > 0.273 && y < 0.364) {
+      result_subtype = "2";   // 
+   } else if (y > 0.364 && y < 0.455) {
+      result_subtype = "3";
+   } else if (y > 0.455 && y < 0.546) {
+      result_subtype = "4";
+   } else if (y > 0.546 && y < 0.637) {
+      result_subtype = "5";
+   } else if (y > 0.637 && y < 0.728) {
+      result_subtype = "6";
+   } else if (y > 0.728 && y < 0.819) {
+      result_subtype = "7";   // 
+   } else if (y > 0.819 && y < 0.910) {
+      result_subtype = "8";
+   } else if (y > 0.910 && y < 1.000) {
+      result_subtype = "9";
+   }
+
+   // print information in result.
+   document.getElementById("result_box").innerHTML = result_type + result_subtype + " " + "V";
+   console.log("Startype:", result_type, " ", " ", "gen.val", "=", x)
+   console.log("Starsubtype:", result_subtype, " ", " ", "gen.val", "=", y)
 
 }
 
+function temp() {
+   document.getElementById("testbox1").addEventListener("input")
+   if (x = "hello") {
+      document.getElementById("testbox2") = "Works!"
+   } else {
+      document.getElementById("testbox2") = "Error"
+   }
+}
